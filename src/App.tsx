91c234efafea17f7d60f747ac6b754eed2ce5506
clemwo <clemwo@mailbox.org>
2024-01-05 12:48:34 +0100

    Set up example project to demonstrate screen flickering

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { VictoryChart, VictoryLine, VictoryTheme } from "victory";
 
-function App() {
+const generateRandomData = (
+  numPoints = 5,
+  xRange = [1, 5],
+  yRange = [1, 50000]
+) => {
+  return Array.from({ length: numPoints }, (_, index) => {
+    return {
+      x: index + xRange[0],
+      y: Math.floor(Math.random() * (yRange[1] - yRange[0] + 1)) + yRange[0],
+    };
+  });
+};
+
+export default function Home() {
+  const [data, setData] = useState(generateRandomData());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setData(generateRandomData());
+    }, 3000);
+
+    return () => clearInterval(intervalId); // Clear interval on component unmount
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <main className="body flex w-screen h-screen bg-white">
+      <div className="debug-red flex w-full h-full">
+        <VictoryChart
+          // doesn't make a difference whether animate is set only here, only on victory line or on both
+          animate={{ duration: 1000 }}
+          theme={VictoryTheme.material}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <VictoryLine
+            animate={{ duration: 1000 }}
+            data={data}
+            style={{
+              data: { stroke: "green", width: 12 },
+            }}
+          />
+        </VictoryChart>
+      </div>
+    </main>
   );
 }
-
-export default App;
